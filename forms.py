224231cdc0e8d3a99f99ee6301aefef9673a4805
Fernando Palacios <deliopalaciosviafara@gmail.com>
2024-11-07 16:37:51 -0500

    Add user authentication and registration functionality

---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
--- main.py
+++ main.py
@@ -6,10 +6,14 @@ to solve programming problems and submit their solutions for evaluation.
 
 """
 import re
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for, flash
+from flask_login import LoginManager, current_user, login_user, logout_user
+from db import db
+from urllib.parse import urlparse
+from User import User
+from forms import LoginForm, SignupForm
 
 app = Flask(__name__)
-
 problems = [{
     'name': 'FizzBuzz',
     'description':
@@ -106,37 +110,67 @@ problems = [{
 
 }]
 
+app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-def validate_forbidden_words(solution_code, forbidden_words):
-    """
-    Validates if the solution code contains forbidden words.
-    """
-    for word in forbidden_words:
-        if word in solution_code:
-            return False, f"Uso de palabra prohibida: {word}"
-    return True, ""
+db.init_app(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 
 
-def check_recursion(solution_code, function_name):
-    """
-    Checks if the solution code uses recursion.
-    """
-    return solution_code.count(f"{function_name}(") >= 2
+@login_manager.user_loader
+def loadUser(user_id):
+    return User.query.get(int(user_id))
 
 
-def is_string(value):
-    """
-    Checks if the value is a string.
-    """
-    return isinstance(value, str)
+@app.before_request
+def create_tables():
+    if not hasattr(app, 'tables_crrated'):
+        db.create_all()
+        app.tables_crrated = True
 
 
-@app.route('/')
-def index():
-    """
-    Endpoint for login.
-    """
-    return 'Endpoint para el login'
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('show_problems'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Nombre de usuario o contraseña inválidos')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlparse(next_page).netloc != '':
+            next_page = url_for('show_problems')
+        return redirect(next_page)
+    return render_template('login.html', title='Ingresar', form=form)
+
+
+@app.route("/signup/", methods=["GET", "POST"])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('show_problems'))
+    form = SignupForm()
+    if form.validate_on_submit():
+        if User.query.filter_by(username=form.username.data).first() is not None:
+            flash('Este nombre de usuario ya existe')
+            return redirect(url_for('signup'))
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Felicitaciones, ahora estás registrado!')
+        return redirect(url_for('login'))
+    return render_template('signup.html', title='Registrarse', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 @app.route('/problems')
@@ -197,6 +231,35 @@ def submit_solution():
         return render_template('evaluation_result.html', result="Error: El lenguaje aún no está soportado :)", problem=problem, is_string=is_string)
 
 
+def validate_forbidden_words(solution_code, forbidden_words):
+    """
+    Validates if the solution code contains forbidden words.
+    """
+    for word in forbidden_words:
+        if word in solution_code:
+            return False, f"Uso de palabra prohibida: {word}"
+    return True, ""
+
+
+def check_recursion(solution_code, function_name):
+    """
+    Checks if the solution code uses recursion.
+    """
+    return solution_code.count(f"{function_name}(") >= 2
+
+
+def is_string(value):
+    """
+    Checks if the value is a string.
+    """
+    return isinstance(value, str)
+
+
+@app.route('/register', methods=['GET'])
+def show_register():
+    return render_template('register.html')
+
+
 def evaluate_python_code(solution_code, test_cases):
     """
     Evaluates the solution code in Python using test cases.
@@ -208,7 +271,7 @@ def evaluate_python_code(solution_code, test_cases):
             exec_globals = {}
             exec(solution_code, exec_globals)
 
-           # Call the solution() function with the test case parameters
+            # Call the solution() function with the test case parameters
             output = exec_globals['solution'](*test_case["input"])
             results.append({
                 "input": test_case["input"],

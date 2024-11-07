7e3d548ce7f96089d2744d2bd8e23566756b6a0b
Fernando Palacios <deliopalaciosviafara@gmail.com>
2024-11-07 17:01:09 -0500

    Remove unused imports

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
